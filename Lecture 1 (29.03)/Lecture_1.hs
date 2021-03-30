a9f62baa41625530d4a01e3f02db1331c49fc9cf
Kamil Khayrullin <Kamil Khayrullin@DESKTOP-FQSSIHS>
2021-03-31 01:01:08 +0300

    first task done.

--- Lecture 1 (29.03)/Lecture_1.hs
+++ Lecture 1 (29.03)/Lecture_1.hs
@@ -2,38 +2,90 @@
 import CodeWorld
 
 -- | Draw a circle for traffic lights given color and Y-offset.
-lightCircle :: Double -> Color -> Picture
-lightCircle y c = translated 0 y (colored c (solidCircle 1))
+lightCircle :: Double -> Double -> Color -> Picture
+lightCircle x y c = translated x y (colored c (solidCircle 1))
+
+pedestriansIcon :: Double -> Double -> Color -> Picture
+pedestriansIcon x y c = translated x y (colored c (lettering  "\x1F6B6"))
+
+cyclistsIcon :: Double -> Double -> Color -> Picture
+cyclistsIcon x y c = translated x y (colored c (lettering  "\x1F6B2"))
 
 -- | Frame for traffic lights.
 border :: Picture
-border = rectangle 2.5 7.5
+border = translated 4 (-1.25) (rectangle 2.5 5) <> rectangle 2.5 7.5 <> translated (-4) (-1.25) (rectangle 2.5 5)
 
 -- | Green traffic light.
 greenLight :: Picture
-greenLight = lightCircle (-2.375) green
+greenLight = lightCircle 0 (-2.375) green
 
 -- | Red traffic light.
 redLight :: Picture
-redLight = lightCircle 2.375 red
+redLight = lightCircle 0 2.375 red
 
 -- | Yellow traffic light.
 yellowLight :: Picture
-yellowLight = lightCircle 0 yellow
+yellowLight = lightCircle 0 0 yellow
 
 -- | Green inactive traffic light.
 greenInactiveLight :: Picture
-greenInactiveLight = lightCircle (-2.375) grey
+greenInactiveLight = lightCircle 0 (-2.375) grey
 
 -- | Yellow inactive traffic light.
 yellowInactiveLight :: Picture
-yellowInactiveLight = lightCircle 0 grey
+yellowInactiveLight = lightCircle 0 0 grey
 
 -- | Top inactive traffic light.
 redInactiveLight :: Picture
-redInactiveLight = lightCircle 2.375 grey
+redInactiveLight = lightCircle 0 2.375 grey
 
+-- | Green traffic light for pedestrians.
+greenLightPedestrians :: Picture
+greenLightPedestrians = lightCircle 4 (-2.375) green
 
+-- | Red traffic light for pedestrians.
+redLightPedestrians :: Picture
+redLightPedestrians = lightCircle 4 0 red
+
+-- | Green inactive traffic light for pedestrians.
+greenInactiveLightPedestrians :: Picture
+greenInactiveLightPedestrians = lightCircle 4 (-2.375) grey
+
+-- | Top inactive traffic light for pedestrians.
+redInactiveLightPedestrians :: Picture
+redInactiveLightPedestrians = lightCircle 4 0 grey
+
+-- | Icon for pedestrians at green light
+pedestriansIconGreen :: Picture
+pedestriansIconGreen = pedestriansIcon 4 (-2.375) black
+
+-- | Icon for pedestrians at red light
+pedestriansIconRed :: Picture
+pedestriansIconRed = pedestriansIcon 4 0 black
+
+-- | Green traffic light for cyclists.
+greenLightCyclists :: Picture
+greenLightCyclists = lightCircle (-4) (-2.375) green
+
+-- | Red traffic light for cyclists.
+redLightCyclists :: Picture
+redLightCyclists = lightCircle (-4) 0 red
+
+-- | Green inactive traffic light for cyclists.
+greenInactiveLightCyclists :: Picture
+greenInactiveLightCyclists = lightCircle (-4) (-2.375) grey
+
+-- | Top inactive traffic light for cyclists.
+redInactiveLightCyclists :: Picture
+redInactiveLightCyclists = lightCircle (-4) 0 grey
+
+-- | Icon for cyclists at green light
+cyclistsIconGreen :: Picture
+cyclistsIconGreen = cyclistsIcon (-4) (-2.375) black
+
+-- | Icon for cyclists at red light
+cyclistsIconRed :: Picture
+cyclistsIconRed = cyclistsIcon (-4) 0 black
 
 -- | Simple traffic lights with four states.
 --
@@ -42,10 +94,10 @@ redInactiveLight = lightCircle 2.375 grey
 -- * '2' — red active, yelow inactive, green inactive
 -- * '3' — red active, yelow active, green inactive
 trafficLights :: Int -> Picture
-trafficLights 0 = redInactiveLight <> yellowInactiveLight <> greenLight <> border
+trafficLights 0 = redInactiveLight <> yellowInactiveLight <> greenLight <> pedestriansIconGreen <> greenLightPedestrians <> redInactiveLightPedestrians  <> cyclistsIconGreen <> greenLightCyclists <> redInactiveLightCyclists <> border
 trafficLights 1 = redInactiveLight <> yellowLight <> greenInactiveLight <> border
-trafficLights 2 = redLight <> yellowInactiveLight <> greenInactiveLight <> border
-trafficLights 3 = redLight <> yellowLight <> greenInactiveLight <> border
+trafficLights 2 = redLight <> yellowInactiveLight <> greenInactiveLight <> pedestriansIconRed <> greenInactiveLightPedestrians <> redLightPedestrians <> cyclistsIconRed <> greenInactiveLightCyclists <> redLightCyclists <> border
+trafficLights 3 = redLight <> yellowLight <> greenInactiveLight <> pedestriansIconRed <> greenInactiveLightPedestrians <> redLightPedestrians  <> cyclistsIconRed <> greenInactiveLightCyclists <> redLightCyclists <> border
 
 -- | Traffic lights controller switching lights every 2 seconds.
 trafficController :: Double  -> Picture
