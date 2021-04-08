57d22f257cdcda1f62a93738292cc8119f91d27d
Kamil Khayrullin <Kamil Khayrullin@DESKTOP-FQSSIHS>
2021-04-08 17:56:37 +0300

    last code

--- Lecture 2 (02.04)/last_code.hs
+++ Lecture 2 (02.04)/last_code.hs
@@ -3,23 +3,72 @@
 
 import CodeWorld
 
-data Tile = Wall | Floor | Door DoorColor  | Exit  | Button deriving (Show, Eq)
+data Tile = Wall | Floor | Door DoorColor  | Exit  | Button DoorColor deriving (Show, Eq)
 data DoorColor = Red | Blue | Green deriving (Show, Eq)
 data Coords = Coords Int Int
 data Dir = LeftMove | RightMove | UpMove | DownMove deriving (Show, Eq)
 
 levelMap :: Coords -> Tile
 levelMap (Coords i j)
-  | (i, j) == (5, 3) = Door Red
-  | j == 3 = Wall
+  | (i, j) == (4, 4) = Door Red
+--  | (i, j) == (1, 1) = Door Blue
+--  | j == 3 = Wall
   | i > 10 = Wall
   | i < -9 = Wall
   | j > 10 = Wall
   | j < -9 = Wall
-  | (i, j) == (2, 3) = Wall
-  | (i, j) == (2, 2) = Button
-  | (i, j) == (3, 2) = Exit
+--  | (i, j) == (2, 3) = Wall
+  | (i, j) == (2, 4) = Button Red
+--  | (i, j) == (3, 2) = Exit
   | otherwise = Floor
+
+openDoors :: [DoorColor] -> (Coords -> Tile) -> (Coords -> Tile)
+openDoors colors currentMap = newMap
+  where
+    newMap :: Coords -> Tile
+    newMap (Coords i j)
+      | currentMap (Coords i j) == (Door dc) && dc `elem` colors = Floor
+      | otherwise = currentMap (Coords i j)
+      where
+        (Door dc) = currentMap (Coords i j)
+
+compareTileAt :: (Int, Int) -> (Coords -> Tile) -> (Coords -> Tile) -> Bool
+compareTileAt (a, b) map1 map2
+  | map1 (Coords a b) == map2 (Coords a b) = True
+  | otherwise = False
+
+compareRows :: (Int, Int) -> Int -> (Coords -> Tile) -> (Coords -> Tile) -> Bool
+compareRows (from, to) j map1 map2
+  | from > to = True
+  | isSame = compareRows (from + 1, to) j map1 map2
+  | otherwise = False
+  where
+    isSame = compareTileAt (from, j) map1 map2
+
+compareMaps :: (Int, Int) -> (Coords -> Tile) -> (Coords -> Tile) -> Bool
+compareMaps (from, to) map1 map2
+  | from > to = True
+  | isSame = compareMaps(from + 1, to) map1 map2
+  | otherwise = False
+  where
+    isSame = compareRows ((-10), 11) from map1 map2
+
+getMap :: (Int, Int) -> (Coords -> Tile) -> (Coords -> Tile)
+getMap (i, j) currentMap
+  | areMapsSame levelMap && isButton = openDoors [dc] currentMap
+  | areMapsSame (openDoors [Red] levelMap)  && isButton = openDoors [Red, dc] currentMap
+  | areMapsSame (openDoors [Green] levelMap) && isButton = openDoors [Green, dc] currentMap
+  | areMapsSame (openDoors [Blue] levelMap) && isButton = openDoors [Blue, dc] currentMap
+  | areMapsSame (openDoors [Red, Green] levelMap) && isButton = openDoors [Red, Green, dc] currentMap
+  | areMapsSame (openDoors [Red, Blue] levelMap) && isButton = openDoors [Red, Blue, dc] currentMap
+  | areMapsSame (openDoors [Blue, Green] levelMap) && isButton = openDoors [Blue, Green, dc] currentMap
+  | otherwise = (openDoors [Red, Green, Blue] levelMap)
+  where
+    areMapsSame :: (Coords -> Tile) -> Bool
+    areMapsSame someMap = compareMaps (-10, 11) someMap currentMap
+    isButton = currentTile == (Button dc)
+    currentTile = levelMap (Coords i j)
+    (Button dc) = currentMap (Coords i j)
 
 doorColor :: DoorColor -> Color
 doorColor Red = red
@@ -29,7 +78,7 @@ doorColor Green = green
 drawTile :: Tile -> Picture
 drawTile Wall = colored black (solidRectangle 0.95 0.95)
 drawTile Floor = colored yellow (solidRectangle 0.95 0.95)
-drawTile Button = colored red (solidCircle 0.45) <> colored yellow (solidRectangle 0.95 0.95)
+drawTile (Button dc) = colored (doorColor dc) (solidCircle 0.45) <> colored yellow (solidRectangle 0.95 0.95)
 drawTile Exit = colored green (solidRectangle 0.95 0.95)
 drawTile (Door dc) = colored (doorColor dc) (solidCircle 0.45) <> colored black (solidRectangle 0.95 0.95)
 
@@ -57,29 +106,23 @@ drawPlayer (Coords i j) = translated x y  (lettering  "\x1F6B6")
 drawLevelMap :: (Coords -> Tile) -> Picture
 drawLevelMap levelmap = drawFromTo (-10, 11) (\j -> drawRow levelmap (-10, 11) j)
 
-openDoors :: DoorColor -> (Coords -> Tile) -> (Coords -> Tile)
-openDoors color currentMap = newMap
-  where
-    newMap :: Coords -> Tile
-    newMap (Coords i j)
-      | currentMap (Coords i j) == Door color = Floor
-      | otherwise = currentMap (Coords i j)
-
 
 tryMove :: Dir -> Coords -> Coords
 tryMove dir (Coords i j)
-  | dir == UpMove && canMove (levelMap (Coords i (j + 1))) = (Coords i (j + 1))
-  | dir == DownMove && canMove (levelMap (Coords i (j - 1))) = (Coords i (j - 1))
-  | dir == LeftMove && canMove (levelMap (Coords (i - 1) j)) = (Coords (i - 1) j)
-  | dir == RightMove && canMove (levelMap (Coords (i + 1) j)) = (Coords (i + 1) j)
+  | dir == UpMove && canMove (currentMap (Coords i (j - 1))) = (Coords i (j + 1))
+  | dir == DownMove && canMove (currentMap (Coords i (j - 1))) = (Coords i (j - 1))
+  | dir == LeftMove && canMove (currentMap (Coords (i - 1) j)) = (Coords (i - 1) j)
+  | dir == RightMove && canMove (currentMap (Coords (i + 1) j)) = (Coords (i + 1) j)
   | otherwise = (Coords i j)
+  where
+    currentMap =  getMap (i , j) levelMap
+
 
 canMove :: Tile -> Bool
-canMove tile
-  | tile == Floor = True
-  | tile == Button = True
-  | tile == Exit = True
-  | otherwise = False
+canMove Floor = True
+canMove (Button dc) = True
+canMove Exit = True
+canMove _any = False
 
 initialWorld :: Coords
 initialWorld = Coords 0 0
@@ -91,10 +134,13 @@ handleWorld (KeyPress "Left") (Coords i j) = tryMove LeftMove (Coords i j)
 handleWorld (KeyPress "Right") (Coords i j) = tryMove RightMove (Coords i j)
 handleWorld _anyEvent coords = coords
 
-renderWorld :: Coords -> Picture
-renderWorld coords = scaled 0.8 0.8 (drawPlayer coords <> drawLevelMap (openDoors Red levelMap))
+renderWorld :: (Coords -> Tile) -> Coords -> Picture
+renderWorld myMap (Coords i j)
+  | compareMaps (-10, 11) myMap (getMap (i, j) myMap) = scaled 0.8 0.8 (drawPlayer (Coords i j)) <> drawLevelMap (myMap)
+  | otherwise = renderWorld (getMap (i, j) myMap) (Coords i j)
+
 
 main :: IO ()
 main = solution1
   where
-    solution1 = activityOf initialWorld handleWorld renderWorld
+    solution1 = activityOf initialWorld handleWorld (renderWorld levelMap)
